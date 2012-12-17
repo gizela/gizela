b2d0ebea83f277da844d411916374b0886994924
Tomas Kubin <tomas.kubin@fsv.cvut.cz>
2012-12-17 11:24:18 +0100

    README in restructured text

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='Gizela',
-    version='0.4.1',
+    version='0.4.3',
     author='Michal Seidl, Tomas Kubin',
     author_email='michal.seidl@fsv.cvut.cz, tomas.kubin@fsv.cvut.cz',
     packages=['gizela',
